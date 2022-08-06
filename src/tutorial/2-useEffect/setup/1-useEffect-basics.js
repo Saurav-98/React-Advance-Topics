d845f3a5a9aff5f32aba046049b4f094ebb5d3ab
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-06 21:12:48 +0530

    Learning useEffect Hook

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,23 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log('Running Use Effect');
+    if (value >= 1) {
+      document.title = `New Messages(${value})`;
+    }
+  });
+
+  return (
+    <>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        Click Here
+      </button>
+    </>
+  );
 };
 
 export default UseEffectBasics;
