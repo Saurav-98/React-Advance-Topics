c2af96b915e5da5b1d3924d01e4a845f8a6ba131
Saurav Kumar Verma <sauravkumarverma22@gmail.com>
2022-08-10 11:56:54 +0530

    normal todo Structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import Setup from './tutorial/4-forms/setup/1-controlled-inputs';
-// import Final from './tutorial/2-useEffect/final/1-useEffect-basics';
+import Setup from './tutorial/6-useReducer/setup';
+
 function App() {
   return (
     <div className="container">
       <Setup />
-      <hr style={{ marginBlock: '2rem' }} />
     </div>
   );
 }
